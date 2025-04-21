62483ff134b7a90078835df371f8fdb4b1e2fd0e
sasindubm <msasindu450@gmail.com>
2025-04-22 03:46:41 +0530

    logut added

--- views/home.php
+++ views/home.php
@@ -9,6 +9,15 @@ include('layouts/header.php');
 <main>
     <h1>Welcome</h1>
     <p>You are successfully logged into home page</p>
+    <form action="home.php" method="get">
+    <input type="submit" value="Log Out" name="logout">
+    </form>
+    <?php
+    if (!empty($_GET["logout"])) {
+        session_destroy();
+        header("Location: login.php");
+    }
+    ?>
 </main>
 <?php
 include('layouts/footer.php');
